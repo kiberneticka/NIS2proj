17126dac3eea49bd2e58cf48a8c12045758cbfc1
kiberneticka <kiberneticka@proton.me>
2025-11-19 20:19:00 +0100

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' // PUTANJA JE ISPRAVNA
 import './index.css'
 import { I18nextProvider } from 'react-i18next'
 import i18next from 'i18next'
-import hr from './locales/hr.json' // Pretpostavlja da hr.json postoji
-import en from './locales/en.json' // Pretpostavlja da en.json postoji
-import de from './locales/de.json' // Pretpostavlja da de.json postoji
-import it from './locales/it.json' // Pretpostavlja da it.json postoji
-import sl from './locales/sl.json' // Pretpostavlja da sl.json postoji
-import es from './locales/es.json' // Pretpostavlja da es.json postoji
+import hr from './locales/hr.json' 
+import en from './locales/en.json' 
+import de from './locales/de.json' 
+import it from './locales/it.json' 
+import sl from './locales/sl.json' 
+import es from './locales/es.json' 
 
 // Dodajte minimalne potrebne prijevode ako ne postoje
 if (!hr.translation) {
@@ -37,15 +37,15 @@ if (!en.translation) {
 
 
 i18next.init({
-  interpolation: { escapeValue: false },
-  lng: 'hr',
-  resources: { hr: { translation: hr.translation }, en: { translation: en.translation }, de: { translation: de.translation }, it: { translation: it.translation }, sl: { translation: sl.translation }, es: { translation: es.translation } }
+    interpolation: { escapeValue: false },
+    lng: 'hr',
+    resources: { hr: { translation: hr.translation }, en: { translation: en.translation }, de: { translation: de.translation }, it: { translation: it.translation }, sl: { translation: sl.translation }, es: { translation: es.translation } }
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <I18nextProvider i18n={i18next}>
-      <App />
-    </I18nextProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <I18nextProvider i18n={i18next}>
+            <App />
+        </I18nextProvider>
+    </React.StrictMode>
 )
